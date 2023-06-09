621b4072fe313f72980c413dd6eac7f89f6e16df
Sambasivam-Kamalini <134925733+Sambasivam-Kamalini@users.noreply.github.com>
2023-06-09 06:13:22 +0530

    Add files via upload

--- robot_builder_assignment01.py
+++ robot_builder_assignment01.py
@@ -16,22 +16,22 @@ def rectangle(horizontal,vertical,color):
     t.bgcolor ('Dodger blue')
 # feet
 t.goto(-100, -150)
-rectangle(50, 20, 'blue')
+rectangle(50, 20, 'navy')
 t.goto(-30, -150)
-rectangle(50, 20, 'blue')
+rectangle(50, 20, 'navy')
 # legs
 t.goto(-25, -50)
-rectangle(15, 100, 'purple')
+rectangle(15, 100, 'maroon')
 t.goto(-55, -50)
-rectangle(-15, 100, 'purple')
+rectangle(-15, 100, 'maroon')
 # body
 t.goto(-90, 100)
-rectangle(100, 150, 'lawn green')
+rectangle(100, 150, 'forest green')
 # arms
 t.goto(-150, 70)
-rectangle(60, 15, 'goldenrod')
+rectangle(60, 15, 'peru')
 t.goto(-150, 110)
-rectangle(15, 40, 'goldenrod')
+rectangle(15, 40, 'peru')
 
 t.goto(10, 70)
 rectangle(60, 15, 'grey')
@@ -42,7 +42,7 @@ t.goto(-50, 120)
 rectangle(15, 20, 'light blue')
 # head
 t.goto(-85, 170)
-rectangle(80, 50, 'yellow')
+rectangle(80, 50, 'gold')
 # eyes
 t.goto(-60, 160)
 rectangle(30, 10, 'white')
